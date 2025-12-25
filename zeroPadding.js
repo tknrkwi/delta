a11e4faa9c8b288dbb3f5815a8d0b6d82e9c32b4
tknrkwi <166661910+tknrkwi@users.noreply.github.com>
2025-12-26 03:41:38 +0900

    ゼロパディング

--- zeroPadding.js
+++ zeroPadding.js
@@ -1,8 +1,8 @@
 /**
  * 指定桁でゼロ埋めする（ゼロパディング）
  * @returns {string}
- * @param   {string} - ゼロ埋め対象の文字列 
- * @param   {number} - ゼロ埋め後の桁数
+ * @param   {string} mojiretu - ゼロ埋め対象の文字列 
+ * @param   {number} keta - ゼロ埋め後の桁数
  */
 function zeroPadding( mojiretu, keta ) {
   return mojiretu.padStart( keta, '0' );
