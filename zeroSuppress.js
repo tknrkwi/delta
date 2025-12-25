b0de7d0ed60c6b4f8de982e70c30072d42f12a50
tknrkwi <166661910+tknrkwi@users.noreply.github.com>
2025-12-26 03:42:19 +0900

    ゼロサプレス

--- zeroSuppress.js
+++ zeroSuppress.js
@@ -1,7 +1,7 @@
 /**
  * ゼロサプレス（ '00123' → '123' )
  * @returns {string}
- * @param   {string} - ゼロサプレス前の文字列（数字であること）
+ * @param   {string} mojiretu - ゼロサプレス前の文字列（数字であること）
  */
 function zeroSuppress( mojiretu ) {
   return +mojiretu.toString();
